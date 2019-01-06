fb5b7953f4f45da70afeaac5c82235ebd29b8231
Tim Speckhals <github@timmyrs.de>
2019-01-06 17:56:51 +0100

    1.0.2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 exports.getEuMembers = function()
 {
-	return ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "EL", "ES", "FR", "HR", "IT", "CY", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "UK"];
+	return ["BE", "BG", "CZ", "DK", "DE", "EE", "IE", "EL", "ES", "FR", "HR", "IT", "CY", "LV", "LT", "LU", "HU", "MT", "NL", "AT", "PL", "PT", "RO", "SI", "SK", "FI", "SE", "GB"];
 };
 exports.isEuMember = function(code)
 {
-	return exports.getEuMembers().indexOf(code) != -1;
+	return exports.getEuMembers().indexOf(code.toUpperCase()) != -1;
 };
